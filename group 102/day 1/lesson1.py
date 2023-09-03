3c1a3aedbd11dba6c826631fd2f1f91748e6b427
juniorcoder19 <lukamaziashvili14@gmail.com>
2023-09-03 04:18:48 +0400

    gakverili 1-s davaleba

---
+++ group 102/day 1/lesson1.py
@@ -0,0 +1,42 @@
+#name = "luka"
+#name არის ცვლადი
+# = არის ცვლადისთვის მინიშნების მინიჭების სიმბოლო
+# "nika" არის ცვლადის მნიშვნელობა
+#surname = "maziashvili"
+
+#print (name, surname)
+# print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი (ცვლადი და სტრინგი)
+
+
+#name = "luka"    #ეს არის str (string) ტიპის ცვლადი
+#age = 19         #ეს არის int (integer) მთელი რიცხვი
+#height = 180.5   #ეს არის float ტიპის ცვლადი (ათწილადი)
+
+
+#boolean (bool) ტიპის ცვლადი
+#knows_programming = True  #(True) or (false)
+
+
+#is_ugly = False #snakecase - (უბრალოდ წერის სტილი სტანდარტულად)
+#isUgly = False  #ჯავასკრიპტული camelcase
+#print(name + " " + surname)
+
+
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+#print(name + " " + str(age))
+
+
+
+name = "გამარჯობა, მე ვარ ლუკა"
+surname = "მაზიაშვილი"
+age = (str(19) + " " + "წლის")
+height = ("სიმაღლეში" + " " + str(180) + " " + "სანტიმეტრი.")
+is_athletic = True
+
+print(name +" " + surname + " " + age + " " + height)
