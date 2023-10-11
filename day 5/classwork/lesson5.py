838516c874b5a3ba375183e685f22a9c401308f0
juniorcoder19 <lukamaziashvili14@gmail.com>
2023-10-11 13:54:46 +0400

    day5

---
+++ day 5/classwork/lesson5.py
@@ -0,0 +1,25 @@
+# #Define the number of iterations
+# for i in range(10):
+#   #Statement that gets repeated
+#   print("Hello")
+
+# for i in range(10):
+#   print(i)
+
+
+
+# name = input("Enter Your Name: ")
+# age = int(input("Enter Your Age: "))
+# years = 2023
+# for i in range(10):
+#     print(name +" "+ "შენ იქნები"+" " + str(age + i) +" "+ "წლის"+" "+ str(years + i)+" " + "წელს")
+
+# for i in range(5):
+#     print("Congrats!")
+
+# for num in range(6):
+#   print(num)
+
+# password = "SecretWord"
+# guess = "1234"
+# print(guess != password)
---
+++ day 5/homework/lesson5.py
@@ -0,0 +1,14 @@
+#### DAVALEBA 
+# # მომხმარებელს კითხეთ თავისი და მამამისის ასაკი, 
+# # დაუპრინტეთ ყოველ წელს რამდენჯერ უფროსი იქნება მამამისი მასზე.
+
+
+# #vtqvat shvili aris 19 wlis
+# age=int(input("Your Age: "))
+
+# #vtqvat mama aris 50 wlis
+# fatherage= int(input("Your Father's Age: "))
+
+# year = 2023
+# for i in range(10):
+#    print("შენი ასაკია"+" "+str(age+i)+" "+ "მამაშნის ასაკია"+" "+str(fatherage+i) +" "+str(year+i)+" "+ "წელს მამაშენი შენზე 31 წლით დიდია")
