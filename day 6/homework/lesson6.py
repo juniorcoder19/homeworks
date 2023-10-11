eebc98c20899878b0db45c7dcadfebdee50d0290
juniorcoder19 <lukamaziashvili14@gmail.com>
2023-10-11 13:52:51 +0400

    day6homework

---
+++ day 6/homework/lesson6.py
@@ -0,0 +1,36 @@
+     #მომხმარებლის ნიშნებისგან გამოანგარიშება საშუალო არითმეტიკული და თუ ცხრაზე მეტი იქნება:
+     #დაუპრინტეთ გილოცავ მატრიცელო შენ გადმოგეცა 300 ლარიანი ბანძი ტოსტერი რასაც შეალიე შენი ცხოვრების წლები
+     #თუ ეს იქნება 5ზე ნაკლები: დაუპრინტე გილოცავ გაექეცი მატრიცას
+    #თუ იქნება 5 იდან 9 მდე: დაუპრინტე YOU ARE MID
+    #თუ იქნება 10ზე მეტი ან 0ზე ნაკლები: დაუპრინტე there is something wrong with you (edited)
+
+# qartuli = int(input("sheni nishani qartulshi: "))
+# matematika = int(input("sheni nishani matematikashi: "))
+# fizika = int(input("sheni nishani fizikashi: "))
+# qimia = int(input("sheni nishani qimiashi: "))
+# sashualo_aritmetikuli = ((qartuli+matematika+fizika+qimia)/4)
+# if 9<sashualo_aritmetikuli<11 :
+#     print("გილოცავ მატრიცელო შენ გადმოგეცა 300 ლარიანი ბანძი ტოსტერი რასაც შეალიე შენი ცხოვრების წლები")
+# elif sashualo_aritmetikuli < 5:
+#     print("გილოცავ გაექეცი მატრიცას")    
+# elif 4<sashualo_aritmetikuli<10:
+#     print("YOU ARE MID")
+# elif sashualo_aritmetikuli<0 or sashualo_aritmetikuli>10:
+#     print("there is something wrong with you")    
+        
+
+
+
+      # დავალება:
+      # მომხმარებელს შეეკითხეთ ხელფასი
+      # თუ 10000ზე მეტია, დაუპრინტეთ, გოა-ში სწავლობდი?
+      # თუ 1000----10000 -ია , დაუპრინტეთ you mid
+      # თუ 1000-ზე დაბალია, დაუპრინტეთ, შემოსულიყავი გოაში, მატრიცელო
+
+# salary = int(input("შენი ხელფასი: "))
+# if salary>10000:
+#     print("გოა-ში სწავლობდი?")
+# elif 1000<salary<10000:
+#     print("YOU MID")
+# elif salary<1000:
+#     print("შემოსულიყავი გოაში, მატრიცელო")        
