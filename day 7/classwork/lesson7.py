8c7d6d7c0013a17095f6006eedb7343dbb858e22
juniorcoder19 <lukamaziashvili14@gmail.com>
2023-10-16 23:28:45 +0400

    homework

---
+++ day 7/classwork/lesson7.py
@@ -0,0 +1,14 @@
+# my_list = ["pica","shaurma","vashli",7,2.5]
+# print(my_list)
+
+
+#installed games
+# games = [
+#   'Soccer', 'Tic Tac Toe', 'Snake',
+#   'Puzzle', 'Rally']
+
+# #taking player's choice as a number input
+# choice = int(input())
+
+# #output the corresponding game
+# print(games[choice])
---
+++ day 7/homework/lesson7.py
@@ -0,0 +1,20 @@
+# დავალება 1:
+# შექმენით სია სადაც გადასცემთ რაზმის ლიდერების სახელს და გვარს     ელემენტებად და გამოიტანთ თქვენი რაზმის ლიდერის სახელს
+
+leaders=["nikoloz filishvili","gabriel molodini","davit tyeshelashvili","davit janezashvili"]
+print(leaders[0])
+
+# დავალება 2:
+# შექმენით თქვენი რაზმის წევრებისგან სია და დაპრინტეთ შუა 5 რაზმის წევრი(slicing)
+
+members=["luka maziashvili","davit qufarashvili","niko andoevi","ali","ilia adamia"]
+print(members[1:4])
+#sul 5 vart da shua 3 davprinte
+
+# დავალება3:
+# შექმენით სია სადაც იქნება რამდენიმე ხილის სახელი და 1 ბოსტნეული,
+# თქვენ უნდა შეცვალოთ ეს 1 ბოსტნეული სხვა რაიმე ხილით და დაპრინტოთ სია
+
+fruits=["apple","watermalon","strawberry","potato"]
+fruits[3]="banana"
+print(fruits)
