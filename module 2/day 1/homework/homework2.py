a428cc4c467008b5fbba297680b12dbf0c42da2d
juniorcoder19 <lukamaziashvili14@gmail.com>
2023-11-07 23:44:23 +0400

    homework(html)

--- module 2/day 1/homework/homework2.py
+++ module 2/day 1/homework/homework2.py
@@ -6,6 +6,11 @@
 # Given [34, -345, -1, 100] your solution will return -345
 # You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
-def find_smallest_int(arr):
-    arr.sort()
-    return arr[0]
+# def find_smallest_int(arr):
+#     arr.sort()
+#     return arr[0]
+
+
+# list = [12,15,51,1,2,0,312]
+# print(min(list))
+# print(max(list))
